646e2078bbdc248087ae4c70157ec7c97971e10d
Elijah James <elijames2301@gmail.com>
2026-02-19 12:19:19 -0500

    write get or create daily puzzle

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import Fastify from "fastify";
+import { getOrCreateDailyPuzzle } from "./services/puzzle";
 
 const app = Fastify();
 
@@ -8,5 +10,15 @@ app.get("/", async () => {
 
 app.listen({ port: 3000 }, () => {
   console.log("API running on port 3000");
+});
+
+app.get("/puzzle/today", async (req, res) => {
+  const puzzle = await getOrCreateDailyPuzzle();
+
+  res.send({
+    date: puzzle.date,
+    // DO NOT send city name to client
+    puzzleId: puzzle.cityId,
+  });
 });
 
